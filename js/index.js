b3672d29a55c780718b0abd603726ae3ff7084af
Volodymyr Romanenko <98472440+Mr-Nihility@users.noreply.github.com>
2022-08-27 15:03:17 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -681,3 +681,30 @@ The function should return true if that is the case, and else false. Letter comp
 // console.log(duplicateCount("aabBcde")); //, 2,"should ignore case"
 // console.log(duplicateCount("Indivisibility")); //, 1
 // console.log(duplicateCount("Indivisibilities")); //, 2, "characters may not be adjacent
+
+// function rowSumOddNumbers(n) {
+//   // TODO
+//   console.log(Math.pow(n, 3));
+
+// }
+
+// console.log(rowSumOddNumbers(1)); // 1;
+
+// console.log(rowSumOddNumbers(42)); //, 74088;
+
+// function findEvenIndex(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (
+//       arr.slice(0, i).reduce((acc, item) => (acc += item), 0) ===
+//       arr.slice(i + 1).reduce((acc, item) => (acc += item), 0)
+//     )
+//       return i;
+//   }
+//   return -1;
+// }
+
+// console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1])); //, 3;
+// console.log(findEvenIndex([1, 100, 50, -51, 1, 1])); //, 1;
+// console.log(findEvenIndex([20, 10, 30, 10, 10, 15, 35])); //, 3;
+// console.log(findEvenIndex([1, 2, 3, 4, 5, 6])); //, -1;
+// console.log(findEvenIndex([20, 10, -80, 10, 10, 15, 35])); //, -1;
